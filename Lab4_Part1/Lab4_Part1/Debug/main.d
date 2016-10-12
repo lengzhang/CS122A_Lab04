96e41cb7c0b74195d72d463d2abcea5d6b655a95
Leng <lengzhang@live.com>
2016-10-11 23:48:42 -0700

    Add

---
+++ Lab4_Part1/Lab4_Part1/Debug/main.d
@@ -0,0 +1,98 @@
+main.d main.o: .././main.c \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdbool.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ C:\Users\user\Documents\Software\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom1284.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/FreeRTOS.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/projdefs.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source/FreeRTOSConfig.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/portable.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\portable\GCC\ATMega323/portmacro.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/mpu_wrappers.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/task.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/list.h \
+ \\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/croutine.h \
+ .././bit.h .././lcd.h .././keypad.h
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdbool.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+C:\Users\user\Documents\Software\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom1284.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+c:\users\user\documents\software\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/FreeRTOS.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/projdefs.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source/FreeRTOSConfig.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/portable.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\portable\GCC\ATMega323/portmacro.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/mpu_wrappers.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/task.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/list.h:
+
+\\Mac\Dropbox\UCRiverside\2016Fall\CS122A\FreeRTOS_Lab\FreeRTOS\Source\include/croutine.h:
+
+.././bit.h:
+
+.././lcd.h:
+
+.././keypad.h:
